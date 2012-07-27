f6e0d919c1fc93d2ea5cba4ba0f31c4d8a470188
samuelmanzini <samuelmanzini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-27 14:15:05 +0000

    git-svn-id: https://plugins.svn.wordpress.org/buddypress-facebook/trunk@578102 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- buddypress-facebook/loader.php
+++ buddypress-facebook/loader.php
@@ -6,17 +6,13 @@ Plugin Name: BuddyPress Facebook
 
 Plugin URI: http://wordpress.org/extend/plugins/buddypress-facebook/
 
-Description: Let your members and groups show their Facebook Button on their profile page and group page. Using just the name after the Facebook URL (www.facebook.com/), the plugin fetches your members and/or groups username and displays their button in the member's/group's header.
+Description: Let your members and groups show their Facebook Button on their profile page and group page.
 
-
-
-If your BuddyPress community is active on Facebook, this plugin is a great tool for boosting communication both on and off your website.
-
-Version: 0.1
+Version: 0.2
 
 Author: Samuel Costa
 
-Author URI: artewebdesign.com.br
+Author URI: http://www.artewebdesign.com.br
 
 License:GPL2
 
